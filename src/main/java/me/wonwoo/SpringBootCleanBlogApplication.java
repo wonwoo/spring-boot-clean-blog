571d0e9eeab6db64a2251d103b3d39798ba20294
wonwoo <aoruqjfu@gmail.com>
2016-09-01 21:36:56 +0900

    엔티티 스캔 추가 및 TEST 코드 삭제

--- src/main/java/me/wonwoo/SpringBootCleanBlogApplication.java
+++ src/main/java/me/wonwoo/SpringBootCleanBlogApplication.java
@@ -2,20 +2,14 @@ package me.wonwoo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 @SpringBootApplication
-@RestController
+@EntityScan(basePackageClasses = {SpringBootCleanBlogApplication.class, Jsr310JpaConverters.class})
 public class SpringBootCleanBlogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootCleanBlogApplication.class, args);
-	}
-
-	@GetMapping
-	public String hello(){
-		return "hello world";
 	}
 }
